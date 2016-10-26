6cded288c1350006697a2f0ef3fb7b861e519fd8
Vigdis Toresen <vtoresen@gmail.com>
2016-10-26 09:17:57 +0000

    import test

--- script.py
+++ script.py
@@ -1,3 +1,9 @@
 print('coffe')
 print('I like coffee')
 print('I really like coffe')
+
+import test
+radius = 1
+circumference = test.circumference_circle(radius)
+area = test.area_circle(radius)
+print('A circle of radius {} cm has a circumference of {} cm and an area of {} cm^2.'.format(radius,circumference,area))
